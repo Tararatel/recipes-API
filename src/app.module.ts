21b7b420e95ffdd53b0d26d56c92c5d320b38ce0
Tararatel <caratel18@mail.ru>
2025-01-08 13:26:32 +0300

    add p1w2d3

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { RecipesModule } from './recipes/recipes.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [RecipesModule],
+  imports: [
+    RecipesModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'), // Указываем путь к папке public
+    }),
+  ],
   controllers: [],
   providers: [],
 })
